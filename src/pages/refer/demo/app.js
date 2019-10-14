b2803b5e356cfe62dd27cb79828d8cadd993fbfc
gopher <gopherhehe@gmail.com>
2019-10-14 17:38:40 +0800

    refer demo

--- src/pages/refer/demo/app.js
+++ src/pages/refer/demo/app.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
 import App from './app.vue'
+import VueI18n from 'vue-i18n'
 
+Vue.use(VueI18n) // 通过插件的形式挂载
+
+const i18n = new VueI18n({
+    locale: 'zh-CN',    // 语言标识
+    messages: {
+      'zh-CN': require('./common/lang/zh'),   // 中文语言包
+      'en-US': require('./common/lang/en')    // 英文语言包
+    }
+})
+ //this.$i18n.locale // 通过切换locale的值来实现语言切换
+/* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   i18n,  // 不要忘记
+//   components: { App }
+// })
 new Vue({
   el: '#app',
+  i18n,
   render: h => h(App)
 })
--- src/pages/refer/demo/app.vue
+++ src/pages/refer/demo/app.vue
@@ -1,17 +1,43 @@
 <template>
   <div>
-    my test
-    <hello></hello>
+    <span v-text="$t('m.music')"></span>
+    <span>{{$t('m.music')}}</span>
+    <button @click="changeLangEvent">中/英</button>
   </div>
 </template>
 
 <script>
-import hello from '@/components/hello.vue'
-// import hello from '../../components/hello.vue'
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 export default {
   // name: 'app',
-  components: {
-    hello
+  data() {
+    return {
+      lang: "zh-CN"
+    }
+  },
+  methods: {
+      changeLangEvent() {
+          this.$confirm('确定切换语言吗?', '提示', {
+              confirmButtonText: '确定',
+              cancelButtonText: '取消',
+              type: 'warning'
+          }).then(() => {
+              if ( this.lang === 'zh-CN' ) {
+                this.lang = 'en-US';
+                this.$i18n.locale = this.lang;//关键语句
+              }else {
+                this.lang = 'zh-CN';
+                this.$i18n.locale = this.lang;//关键语句
+              }
+          }).catch(() => {
+              this.$message({
+                  type: 'info',
+              });          
+          });
+      }
   }
 }
 </script>
